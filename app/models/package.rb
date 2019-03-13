0afe24bcaf6be8801aca1c3fd0a36a611a75064c
sergiomaia <sergiomaia@gmail.com>
2019-03-12 23:40:26 -0300

    Adjust package class

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,6 +1,6 @@
 class Package < ApplicationRecord
-  belongs_to :customer
-  belongs_to :gadget
+  belongs_to :customer, optional: true
+  belongs_to :gadget, optional: true
 
   enum plan: { top_full_hd: 0 }
 end
