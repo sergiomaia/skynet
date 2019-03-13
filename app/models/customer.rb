facb92576271b29d12ac02cc7d21954590cde792
sergiomaia <sergiomaia@gmail.com>
2019-03-12 23:40:15 -0300

    Adjust customer class

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,9 +3,12 @@ class Customer < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates_associated :packages
 
   belongs_to :user
-  has_many :packages
+  has_many :packages, dependent: :destroy
   has_many :comments
   has_many :gadgets, through: :packages
+
+  accepts_nested_attributes_for :packages, allow_destroy: true
 end
