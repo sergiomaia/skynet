4035a1292d6301a6ca8688f8f824313bfc648157
sergiomaia <sergiomaia@gmail.com>
2019-03-14 09:31:40 -0300

    Added proper scope to welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @customers = Customer.all
+    @late_customers = Customer.includes(:packages).where('packages.expires_at < ?', Date.current).references(:packages)
+    @late_today_customers = Customer.includes(:packages).where('packages.expires_at = ?', Date.current).references(:packages)
   end
 end
