f0649b3f6a5ce2cd74f4c135913f9d211737f11a
sergiomaia <sergiomaia@gmail.com>
2019-03-16 21:35:35 -0300

    Added last registered customers to home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @last_registered_customers = Customer.limit(10).order('created_at desc')
     @late_customers = Customer.includes(:packages).where('packages.expires_at < ?', Date.current).references(:packages)
     @late_today_customers = Customer.includes(:packages).where('packages.expires_at = ?', Date.current).references(:packages)
   end
