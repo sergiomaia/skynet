9d969a6d57a13fcead0cc76a4987dba3b9236e5a
sergiomaia <sergiomaia@gmail.com>
2019-03-16 21:13:06 -0300

    Adjust comments migration

--- db/migrate/20190310131334_create_comments.rb
+++ db/migrate/20190310131334_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments, id: :uuid do |t|
       t.text :body
-      t.integer :user_id
-      t.integer :customer_id
+      t.references :user, type: :uuid
+      t.references :customer, type: :uuid
 
       t.timestamps
     end
