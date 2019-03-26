10cbb1ddd4eb712b991ea89cfa37622a9b281705
gyordanoreis <gyordano@nexoos.com.br>
2019-03-26 00:20:47 -0300

    also respective routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :gadgets
+  resources :gadget_allocations
 
   resources :customers do
     resources :comments, only: [:create, :destroy]
