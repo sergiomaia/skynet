159a7578536a8308c776977482ba619206399acd
sergiomaia <sergiomaia@gmail.com>
2019-03-26 08:42:13 -0300

    Adjust identation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,10 @@ class Customer < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: VALID_EMAIL_REGEX }
   validates_associated :packages
 
   belongs_to :user
