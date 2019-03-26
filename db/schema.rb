51e6a28922ebdebe42109291a4991543f97e60d7
sergiomaia <sergiomaia@gmail.com>
2019-03-25 22:17:22 -0300

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_002153) do
+ActiveRecord::Schema.define(version: 2019_03_26_005646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -69,6 +69,17 @@ ActiveRecord::Schema.define(version: 2019_03_22_002153) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_payments_on_customer_id"
+  end
+
+  create_table "protocols", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id"
+    t.uuid "customer_id"
+    t.string "number"
+    t.string "call_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_protocols_on_customer_id"
+    t.index ["user_id"], name: "index_protocols_on_user_id"
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
