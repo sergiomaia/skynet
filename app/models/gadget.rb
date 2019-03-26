a02436a30e1fb202d9713a1c6065728c2bbba125
sergiomaia <sergiomaia@gmail.com>
2019-03-26 08:42:23 -0300

    Adjust indentation

--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -2,5 +2,9 @@ class Gadget < ApplicationRecord
   belongs_to :customer, optional: true
 
   validates :card, :nds, :status, presence: true
-  enum status: { inactive: 0, active: 1 }
+  
+  enum status: {
+    inactive: 0,
+    active: 1
+  }
 end
