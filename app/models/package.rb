8b8dad91baf1da29d0d00d6c9607ba26ccc287d8
sergiomaia <sergiomaia@gmail.com>
2019-03-26 08:42:32 -0300

    Adjust indentation

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,7 +1,10 @@
 class Package < ApplicationRecord
   belongs_to :customer, optional: true
-  belongs_to :gadget, optional: true
 
   enum plan: { top_full_hd: 0 }
-  enum status: { ativa: 0, inativa: 1}
+
+  enum status: {
+    ativa: 0,
+    inativa: 1
+  }
 end
