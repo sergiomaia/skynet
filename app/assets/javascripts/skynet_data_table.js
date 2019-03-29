b8d09cd10bf4ee821670989f36faac5ea4604c59
gyordanoreis <gyordano@nexoos.com.br>
2019-03-29 04:19:56 -0300

    commented until its fixed

--- app/assets/javascripts/skynet_data_table.js
+++ app/assets/javascripts/skynet_data_table.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
-  if (!!$('.table-skynet-index')) {
-    $('.table-skynet-index').DataTable({
-      "language": {
-        "lengthMenu": "Mostrando _MENU_ entradas por página",
-        "zeroRecords": "Desculpe - nenhuma entrada foi encontrada",
-        "info": "Mostrando página _PAGE_ de _PAGES_",
-        "infoEmpty": "Não há entradas",
-        "infoFiltered": "(filtrado de _MAX_ entradas ao total)",
-        "loadingRecords": "Carregando...",
-        "processing":     "Processando...",
-        "search":         "Buscar:",
-        "paginate": {
-            "first":      "Primeiro",
-            "last":       "Último",
-            "next":       "Próximo",
-            "previous":   "Anterior"
-        },
-      }
-    });
-  }
-});
+// $(document).ready(function() {
+//   if (!!$('.table-skynet-index')) {
+//     $('.table-skynet-index').DataTable({
+//       "language": {
+//         "lengthMenu": "Mostrando _MENU_ entradas por página",
+//         "zeroRecords": "Desculpe - nenhuma entrada foi encontrada",
+//         "info": "Mostrando página _PAGE_ de _PAGES_",
+//         "infoEmpty": "Não há entradas",
+//         "infoFiltered": "(filtrado de _MAX_ entradas ao total)",
+//         "loadingRecords": "Carregando...",
+//         "processing":     "Processando...",
+//         "search":         "Buscar:",
+//         "paginate": {
+//             "first":      "Primeiro",
+//             "last":       "Último",
+//             "next":       "Próximo",
+//             "previous":   "Anterior"
+//         },
+//       }
+//     });
+//   }
+// });
