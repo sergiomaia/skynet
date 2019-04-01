ac45ae3f44e5860f914c5524672c949e56efceae
sergiomaia <sergiomaia@gmail.com>
2019-04-01 12:47:14 -0300

    Added fields to payments table

--- db/migrate/20190322002153_create_payments.rb
+++ db/migrate/20190322002153_create_payments.rb
@@ -2,7 +2,9 @@ class CreatePayments < ActiveRecord::Migration[5.2]
   def change
     create_table :payments, id: :uuid do |t|
       t.boolean :paid, default: false
+      t.float :paid_value
       t.date :paid_at
+      t.references :package, type: :uuid
       t.references :customer, type: :uuid
 
       t.timestamps
