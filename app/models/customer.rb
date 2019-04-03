337761de51eb0b0fe2152f347d393aa61fed4561
sergiomaia <sergiomaia@gmail.com>
2019-04-02 22:57:42 -0300

    Fix relationship

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,9 +11,10 @@ class Customer < ApplicationRecord
   belongs_to :user
   has_many :protocols, dependent: :destroy
   has_many :packages, dependent: :destroy
+  has_many :payments, through: :packages
   has_many :comments, dependent: :destroy
-  has_many :payments, dependent: :destroy
   has_many :gadgets
+
 
   accepts_nested_attributes_for :packages, allow_destroy: true
 end
