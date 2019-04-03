44859353eeca69eadc753026576f5fe6f6f80827
sergiomaia <sergiomaia@gmail.com>
2019-04-02 22:57:26 -0300

    Comment it

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,28 +1,28 @@
-class PaymentsController < ApplicationController
-  before_action :set_payment, only: [:create, :destroy]
+# class PaymentsController < ApplicationController
+#   before_action :set_payment, only: [:create, :destroy]
 
-  def create
-    @payment = Payment.new(payment_params.merge(user: current_user))
-    if @payment.save
-      redirect_back(fallback_location: root_path, notice: 'O Pagamento foi atualizado com sucesso.')
-    else
-      redirect_back(fallback_location: root_path, notice: @payment.errors.full_messages.join(', '))
-    end
-  end
+#   def create
+#     @payment = Payment.new(payment_params.merge(user: current_user))
+#     if @payment.save
+#       redirect_back(fallback_location: root_path, notice: 'O Pagamento foi atualizado com sucesso.')
+#     else
+#       redirect_back(fallback_location: root_path, notice: @payment.errors.full_messages.join(', '))
+#     end
+#   end
 
-  def destroy
-    @customer = @payment.customer
-    @payment.destroy
-    redirect_to @payment, notice: 'O comentário foi removido.'
-  end
+#   def destroy
+#     @customer = @payment.customer
+#     @payment.destroy
+#     redirect_to @payment, notice: 'O comentário foi removido.'
+#   end
 
-  private
+#   private
 
-  def set_payment
-    @payment = current_user.payments.find(params[:id])
-  end
+#   def set_payment
+#     @payment = current_user.payments.find(params[:id])
+#   end
 
-  def payments_params
-    params.require(:payment).permit(:paid, :paid_value, :paid_at, :customer_id, :package_id)
-  end
-end
+#   def payments_params
+#     params.require(:payment).permit(:status, :expires_at_day, :value, :paid_at, :package_id)
+#   end
+# end
