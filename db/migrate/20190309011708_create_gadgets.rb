d1981df6bba231eeb65ff9ce4f892b3915bec346
sergiomaia <sergiomaia@gmail.com>
2019-04-04 22:00:00 -0300

    Added user references to gadgets

--- db/migrate/20190309011708_create_gadgets.rb
+++ db/migrate/20190309011708_create_gadgets.rb
@@ -2,6 +2,7 @@ class CreateGadgets < ActiveRecord::Migration[5.2]
   def change
     create_table :gadgets, id: :uuid do |t|
       t.references :customer, type: :uuid
+      t.references :user, type: :uuid
       t.string  :card
       t.string  :nds
       t.string  :model
