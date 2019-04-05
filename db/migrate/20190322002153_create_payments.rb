fe73359941d1a9cbfee5615951122c7871163099
sergiomaia <sergiomaia@gmail.com>
2019-04-04 22:00:12 -0300

    Fix migration

--- db/migrate/20190322002153_create_payments.rb
+++ db/migrate/20190322002153_create_payments.rb
@@ -1,11 +1,12 @@
 class CreatePayments < ActiveRecord::Migration[5.2]
   def change
     create_table :payments, id: :uuid do |t|
-      t.integer :status
-      t.date :expires_at
+      t.boolean :paid, default: false
       t.float :value
       t.date :paid_at
+      t.date :expires_at
       t.references :package, type: :uuid
+      t.references :user, type: :uuid
 
       t.timestamps
     end
