58a90d6bd6920d3b5941ac397ffca9fbf41c32eb
gyordanoreis <gyordano@nexoos.com.br>
2019-04-06 14:32:53 -0300

    add homepage

--- app/assets/javascripts/skynet_data_table.js
+++ app/assets/javascripts/skynet_data_table.js
@@ -19,4 +19,28 @@ $( document ).on('turbolinks:load', function() {
       }
     });
   }
+
+  if (!!$('.table-skynet-index')) {
+    $('.table-skynet-index-simple').DataTable({
+      "lengthChange": false,
+      "pageLength": 15,
+      "searching": false,
+      "language": {
+        "lengthMenu": "Mostrando _MENU_ entradas por página",
+        "zeroRecords": "Desculpe - nenhuma entrada foi encontrada",
+        "info": "Mostrando página _PAGE_ de _PAGES_",
+        "infoEmpty": "Não há entradas",
+        "infoFiltered": "(filtrado de _MAX_ entradas ao total)",
+        "loadingRecords": "Carregando...",
+        "processing":     "Processando...",
+        "search":         "Buscar:",
+        "paginate": {
+            "first":      "Primeiro",
+            "last":       "Último",
+            "next":       "Próximo",
+            "previous":   "Anterior"
+        },
+      }
+    });
+  }
 })
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @last_registered_customers = Customer.limit(10).order('created_at desc')
+    @last_registered_customers = Customer.limit(15).order('created_at desc')
     @late_customers = Customer.includes(:packages).where('packages.expires_at < ?', Date.current).references(:packages)
     @late_today_customers = Customer.includes(:packages).where('packages.expires_at = ?', Date.current).references(:packages)
+    @birthday_today_customers = Customer.where(birthdate: Date.current.beginning_of_month..Date.current.end_of_month)
   end
 end
