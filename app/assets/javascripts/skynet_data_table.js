4ba8d7b3859ad503ea9c3f32543b5d70752e89f9
gyordanoreis <gyordano@nexoos.com.br>
2019-04-06 15:11:01 -0300

    welcome more simple

--- app/assets/javascripts/skynet_data_table.js
+++ app/assets/javascripts/skynet_data_table.js
@@ -20,9 +20,34 @@ $( document ).on('turbolinks:load', function() {
     });
   }
 
-  if (!!$('.table-skynet-index')) {
+  if (!!$('.table-skynet-index-simple')) {
     $('.table-skynet-index-simple').DataTable({
       "lengthChange": false,
+      "pageLength": 15,
+      "searching": false,
+      "language": {
+        "lengthMenu": "Mostrando _MENU_ entradas por página",
+        "zeroRecords": "Desculpe - nenhuma entrada foi encontrada",
+        "info": "Mostrando página _PAGE_ de _PAGES_",
+        "infoEmpty": "Não há entradas",
+        "infoFiltered": "(filtrado de _MAX_ entradas ao total)",
+        "loadingRecords": "Carregando...",
+        "processing":     "Processando...",
+        "search":         "Buscar:",
+        "paginate": {
+            "first":      "Primeiro",
+            "last":       "Último",
+            "next":       "Próximo",
+            "previous":   "Anterior"
+        },
+      }
+    });
+  }
+
+  if (!!$('.table-skynet-index-simplest')) {
+    $('.table-skynet-index-simplest').DataTable({
+      "lengthChange": false,
+      "paging": false,
       "pageLength": 15,
       "searching": false,
       "language": {
