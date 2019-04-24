c299d2bd20c1f9c00ae2864730522f27ea0b72b8
sergiomaia <sergiomaia@gmail.com>
2019-04-24 09:07:42 -0300

    Adjust service

--- app/services/payments/create_payment_service.rb
+++ app/services/payments/create_payment_service.rb
@@ -3,7 +3,7 @@ module Payments
     def self.create_payment(package, current_user)
       Payment.create!(
         paid: false,
-        expires_at: nil,
+        expires_at_day: package.expires_at_day,
         value: package.value,
         paid_at: nil,
         package_id: package.id,
