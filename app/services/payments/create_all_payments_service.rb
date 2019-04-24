862909b9f231a2eadc71e1707c376568285981b5
sergiomaia <sergiomaia@gmail.com>
2019-04-24 09:07:30 -0300

    Adjust service

--- app/services/payments/create_all_payments_service.rb
+++ app/services/payments/create_all_payments_service.rb
@@ -9,15 +9,22 @@ module Payments
     end
 
     private
+
     def execute
       Payment.create!(
         paid: false,
-        expires_at: nil,
+        expires_at_day: package.payments.last.expires_at_day,
         value: package.value,
         paid_at: nil,
         package_id: package.id,
         user_id: user.id
       )
+
+      update_payment_month
+    end
+
+    def update_payment_month
+      Payment.update(payment_month: Date.current.month)
     end
   end
 end
