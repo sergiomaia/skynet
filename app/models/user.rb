7509e18d843c723c0592636a4a37d79d765a06b7
sergiomaia <sergiomaia@gmail.com>
2019-04-27 18:18:21 -0300

    Set first user as admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  after_create :set_user_as_admin, on: :create
+
   has_many :customers, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :packages, through: :customers
@@ -11,4 +13,12 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :payments, dependent: :destroy
   has_many :gadgets, dependent: :destroy
+
+  private
+
+  def set_user_as_admin
+    if User.count == 1
+      self.update(admin: true)
+    end
+  end
 end
