de216ee9c749ebc4703ed0354f00f13cbf6dc805
sergiomaia <sergiomaia@gmail.com>
2019-04-27 18:18:35 -0300

    Added admin field to users

--- db/migrate/20190310130102_devise_create_users.rb
+++ db/migrate/20190310130102_devise_create_users.rb
@@ -11,6 +11,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
+      ## Customs
+      t.boolean :admin, default: false
+
       ## Rememberable
       t.datetime :remember_created_at
 
