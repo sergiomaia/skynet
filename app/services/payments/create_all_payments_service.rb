688ec191c23ccb1bc6f5ecf59a0c14a064062673
sergiomaia <sergiomaia@gmail.com>
2019-04-26 22:17:48 -0300

    Refactor create all payments service

--- app/services/payments/create_all_payments_service.rb
+++ app/services/payments/create_all_payments_service.rb
@@ -13,18 +13,13 @@ module Payments
     def execute
       Payment.create!(
         paid: false,
-        expires_at_day: package.payments.last.expires_at_day,
+        expires_at: package.expires_at,
+        expires_at_day: package.expires_at.day,
         value: package.value,
         paid_at: nil,
         package_id: package.id,
         user_id: user.id
       )
-
-      update_payment_month
-    end
-
-    def update_payment_month
-      Payment.update(payment_month: Date.current.month)
     end
   end
 end
